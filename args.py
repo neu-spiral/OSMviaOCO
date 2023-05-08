f18552f8b87eafb3ab79452a755bbcb85365c797
gozde-ozcan <gozcan@ece.neu.edu>
2023-05-08 04:32:54 -0400

    main.py is updated with new policies

--- args.py
+++ args.py
@@ -13,7 +13,7 @@ def create_parser():
     parser.add_argument('--partitions', default=None,
                         help='Partitions file to generate constraints from')
     parser.add_argument('--policy', default='OGD', type=str, help='Type of the algorithm',
-                        choices=['OGD', 'BanditOGD', 'KKL', 'whatever'])  # policies?
+                        choices=['OGD', 'OGA', 'OMD', 'Optimistic', 'KKL'])  # policies
     parser.add_argument('--eta', default=0.1, type=float, help='eta of the policy')  # default eta?
     parser.add_argument('--k', default=1, type=int, help='cardinality k for each partition')
     parser.add_argument('--T', default=100, type=int,
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from helpers import save, load
 from ProblemInstances import InfluenceMaximization, FacilityLocation
-from oco_tools import ThresholdObjective, ZeroOneDecisionSet, RelaxedPartitionMatroid, OCOPolicy
-
+from oco_tools import ThresholdObjective, ZeroOneDecisionSet, RelaxedPartitionMatroid, OCOPolicy, OGA, \
+    ShiftedNegativeEntropyOMD, OptimisticPolicy
 from KKL.translate import Translator
 from KKL.mapping import WDNFMapping
 from KKL.offline_alg import ApproxGreedy
@@ -18,26 +18,26 @@ import pickle
 import sys
 
 if __name__ == "__main__":
-    
+
     parser = args.create_parser()
     args = parser.parse_args()
-    
+
     eta = args.eta
     logging.basicConfig(level=logging.INFO)
 
     ### LOAD OR GENERATE THE PROBLEM INSTANCE
-    #use here if the problem instance has already been created
+    # use here if the problem instance has already been created
     if args.problem is not None:
         newProblem = load(args.problem)
         args.problemType = args.problem.split("_")[0].split("/")[-1]
         args.input = args.problem.split("_")[1] + "_" + args.problem.split("_")[2] + "_" + args.problem.split("_")[3]
         args.constraints = int(args.problem.split("_")[-1])
 
-    #use here to generate a new problem instance given the input and the partitions file
+    # use here to generate a new problem instance given the input and the partitions file
     else:
         if args.problemType == 'FL':
             logging.info('Loading movie ratings...')
-            bipartite_graph = load(args.input) # this needs one single bipartite graph
+            bipartite_graph = load(args.input)  # this needs one single bipartite graph
             bipartite_graph = bipartite_graph[0]
             target_partitions = load(args.partitions)
             k_list = dict.fromkeys(target_partitions.keys(), args.k)
@@ -48,75 +48,80 @@ if __name__ == "__main__":
             cardinalities_k = list(k_list.values())
             sets_S = list(target_partitions.values())
             sets_S = [list(sets_S[i]) for i in range(len(sets_S))]
-            new_decision_set = RelaxedPartitionMatroid(newProblem.size, cardinalities_k, sets_S) ##make sure the k_list and target_partiotions formats fit to cardinalities_k, sets_S format
+            new_decision_set = RelaxedPartitionMatroid(newProblem.size, cardinalities_k,
+                                                       sets_S)  ##make sure the k_list and target_partiotions formats fit to cardinalities_k, sets_S format
             logging.info('...done. %d seeds will be selected from each partition.' % args.k)
 
         elif args.problemType == 'IM':
             print("IM problem")
             logging.info('Loading cascades...')
-            graphs = load(args.input) # this needs a list of graphs
+            graphs = load(args.input)  # this needs a list of graphs
             if args.partitions is not None:
                 target_partitions = load(args.partitions)
                 k_list = dict.fromkeys(target_partitions.keys(), args.k)
             else:
                 target_partitions = None
                 k_list = args.k
-            logging.info('...done. Just loaded %d cascades.' %(len(graphs)))
+            logging.info('...done. Just loaded %d cascades.' % (len(graphs)))
             logging.info('Defining an InfluenceMaximization problem...')
             newProblem = InfluenceMaximization(graphs, k_list, target_partitions)
             if args.policy != 'KKL':
                 cardinalities_k = list(k_list.values())
                 sets_S = list(target_partitions.values())
                 sets_S = [list(sets_S[i]) for i in range(len(sets_S))]
-                new_decision_set = RelaxedPartitionMatroid(newProblem.problemSize, cardinalities_k, sets_S) 
+                new_decision_set = RelaxedPartitionMatroid(newProblem.problemSize, cardinalities_k, sets_S)
             logging.info('...done. %d seeds will be selected from each partition.' % args.k)
 
-        #generate a file for problems if it does not already exists
+        # generate a file for problems if it does not already exists
         problem_dir = "problems/"
         if not os.path.exists(problem_dir):
             os.makedirs(problem_dir)
-        
-        #save the newly generated problem instance
+
+        # save the newly generated problem instance
         save(problem_dir + args.problemType + "_" + args.input.split("/")[-1] + "_k_" + str(args.k),
              newProblem)
 
-    
     ### GENERATE THE ThresholdObjective OBJECT
-    new_objective = newProblem.translate() ## TODO test what new_objective returns
+    new_objective = newProblem.translate()  ## TODO test what new_objective returns
     # print(new_objective[0].params)
-    
-    
+
     ### CREATE THE OUTPUT DIRECTORY TO SAVE THE RESULTS IF NOT ALREADY EXISTS
     output_dir = "results/" + args.policy + "/" + args.problemType + "/" + args.input.split("/")[-1] + "/k_" \
-                       + str(args.k) + "_" + str(args.T) + "_iter" + "/"
+                 + str(args.k) + "_" + str(args.T) + "_iter" + "/"
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
         logging.info('...output directory is created...')
     sys.stderr.write('output directory is:' + output_dir)
-    
+
     frac_output = output_dir + 'fractional'
     int_output = output_dir + 'integral'
-    
 
-    ### GENERATE THE OCOPolicy OBJECT
+    # GENERATE THE OCOPolicy OBJECT
     if args.policy == 'OGD':
         newPolicy = OCOPolicy(new_decision_set, eta, new_objective[0])
         logging.info("An Online Gradient Descent policy is generated.")
-        
-    elif args.policy == 'BanditOGD':
-        newPolicy = BanditOGDPolicy(new_decision_set, eta, new_objective)  # TODO this class is not defined yet
-        logging.info("A Bandit Online Gradient Descent policy is generated.")
-        pass
+
+    elif args.policy == 'OGA':
+        newPolicy = OGA(new_decision_set, eta, new_objective)
+        logging.info("An OGA policy is generated.")
+
+    elif args.policy == 'OMD':
+        newPolicy = ShiftedNegativeEntropyOMD(new_decision_set, eta, new_objective)
+        logging.info("A Shifted Negative Entropy Online Mirror Descent policy is generated.")
+
+    elif args.policy == 'Optimistic':
+        newPolicy = OptimisticPolicy(new_decision_set, eta, new_objective)
+        logging.info("An Optimistic policy is generated.")
 
     elif args.policy == 'KKL':
         logging.info("A KKL policy is generated.")
         translator = Translator(newProblem)
         ws = translator.ws
-        n = translator.n # dimension of action s
-        m = translator.m # dimension of Phi(s)
-        T = translator.T # horizon
-        sign = translator.sign # sign used in the wdnf functions
+        n = translator.n  # dimension of action s
+        m = translator.m  # dimension of Phi(s)
+        T = translator.T  # horizon
+        sign = translator.sign  # sign used in the wdnf functions
         index_to_set = translator.index_to_set
         ground_set = newProblem.groundSet
 
@@ -126,10 +131,10 @@ if __name__ == "__main__":
         approx_alg = ApproxGreedy(linear_solver, mapping, initial_point, n)
 
         # set constants
-        W = max([np.linalg.norm(w) for w in ws]) # ||w|| <= W
-        R = np.sqrt(m) # ||Phi(s)|| <= R
+        W = max([np.linalg.norm(w) for w in ws])  # ||w|| <= W
+        R = np.sqrt(m)  # ||Phi(s)|| <= R
         alpha = math.e
-        delta = (alpha + 1) * R**2 / T
+        delta = (alpha + 1) * R ** 2 / T
         eta = (alpha + 1) * R / (W * np.sqrt(T))
 
         # initialize online algorithm
@@ -139,7 +144,7 @@ if __name__ == "__main__":
         game = Game(alg, mapping, ws, n, T)
 
     elif args.policy == 'whatever':  # TODO change with policy names
-        newPolicy = OCOPolicy(new_decision_set, eta, new_objective) 
+        newPolicy = OCOPolicy(new_decision_set, eta, new_objective)
         logging.info("An Online Convex Optimization policy is generated.")
         pass
 
@@ -147,16 +152,16 @@ if __name__ == "__main__":
         ## RUN THE OCOPolicy
         while newPolicy.current_iteration < args.T:
             # TODO design backups if the algorithm is interrupted
-            logging.info("Running iteration #{x}...\n".format(x=newPolicy.current_iteration)) ## TODO format string
+            logging.info("Running iteration #{x}...\n".format(x=newPolicy.current_iteration))  ## TODO format string
             newPolicy.step()
         logging.info("The algorithm is finished.")
-        
-        #SAVE THE RESULTS OF THE OCOPolicy
+
+        # SAVE THE RESULTS OF THE OCOPolicy
         final_frac_rewards = newPolicy.frac_rewards
         final_int_rewards = newPolicy.int_rewards
         print("frac rewards: " + str(final_frac_rewards))
         print("int rewards: " + str(final_int_rewards))
-    
+
         save(frac_output, final_frac_rewards)
         save(int_output, final_int_rewards)
         logging.info("The rewards are saved to: " + output_dir + ".")
